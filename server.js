5cf6b86aafcadf16f8929089537fb9026b1fdfb4
LovepreetSingh24 <lovepreet.singh@att.net>
2024-02-07 21:09:12 -0800

    fixed routes

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
-const routes = require('./routes');
 const sequelize = require('./config/connection');
+
+const productRoutes = require('./routes/api/product-routes');
+const categoryRoutes = require('./routes/api/category-routes');
+const tagRoutes = require('./routes/api/tag-routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
+
+app.use('/api/products', productRoutes);
+app.use('/api/categories', categoryRoutes);
+app.use('/api/tags', tagRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the E-commerce API!');
+});
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
